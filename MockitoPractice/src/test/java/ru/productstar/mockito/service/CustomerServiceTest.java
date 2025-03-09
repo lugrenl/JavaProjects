b126f91aad145b393ae3b168e53f7e6ec5f2d5fc
LugrenLaxx <lugrenl@gmail.com>
2025-03-10 00:16:17 +0300

    MockitoPractice_add_CST_fix

--- MockitoPractice/src/test/java/ru/productstar/mockito/service/CustomerServiceTest.java
+++ MockitoPractice/src/test/java/ru/productstar/mockito/service/CustomerServiceTest.java
@@ -53,14 +53,14 @@ public class CustomerServiceTest {
         when(cr.size()).thenReturn(1);
         assertEquals(1, cr.size());
 
-        ArgumentCaptor<Customer> customerArgumentCaptor = ArgumentCaptor.forClass(Customer.class);
-        verify(cr, times(1)).add(customerArgumentCaptor.capture());
-        Customer customer = customerArgumentCaptor.getValue();
-        assertEquals("Oleg", customer.getName());
-
         InOrder inOrder = inOrder(cr);
         inOrder.verify(cr).getByName("Oleg");
         inOrder.verify(cr).add(any(Customer.class));
         inOrder.verify(cr).size();
+
+        ArgumentCaptor<Customer> customerArgumentCaptor = ArgumentCaptor.forClass(Customer.class);
+        verify(cr, times(1)).add(customerArgumentCaptor.capture());
+        Customer customer = customerArgumentCaptor.getValue();
+        assertEquals("Oleg", customer.getName());
     }
 }
