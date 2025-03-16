91742e7b7504b742235a7b174973ae2b656a17ac
LugrenLaxx <lugrenl@gmail.com>
2025-03-16 21:02:34 +0300

    UnitTest_add_assertAll

--- IsPrime/src/test/java/IsPrimeTest.java
+++ IsPrime/src/test/java/IsPrimeTest.java
@@ -27,21 +27,35 @@ class IsPrimeTest {
 
     @Test
     void CompositeNumbersShouldNotBePrime() {
-        assertFalse(IsPrime.isPrime(4), "4 should not be prime");
-        assertFalse(IsPrime.isPrime(6), "6 should not be prime");
-        assertFalse(IsPrime.isPrime(8), "8 should not be prime");
-        assertFalse(IsPrime.isPrime(9), "9 should not be prime");
-        assertFalse(IsPrime.isPrime(10), "10 should not be prime");
-        assertFalse(IsPrime.isPrime(12), "12 should not be prime");
+        assertAll(
+                () -> assertFalse(IsPrime.isPrime(4), "4 should not be prime"),
+                () -> assertFalse(IsPrime.isPrime(6), "6 should not be prime"),
+                () -> assertFalse(IsPrime.isPrime(8), "8 should not be prime"),
+                () -> assertFalse(IsPrime.isPrime(9), "9 should not be prime"),
+                () -> assertFalse(IsPrime.isPrime(10), "10 should not be prime"),
+                () -> assertFalse(IsPrime.isPrime(12), "12 should not be prime"),
+                () -> assertFalse(IsPrime.isPrime(1024), "1024 should not be prime"),
+                () -> assertFalse(IsPrime.isPrime(4096), "4096 should not be prime"),
+                () -> assertFalse(IsPrime.isPrime(8192), "8192 should not be prime"),
+                () -> assertFalse(IsPrime.isPrime(16384), "16384 should not be prime"),
+                () -> assertFalse(IsPrime.isPrime(1003412), "1003412 should not be prime")
+                );
     }
 
     @Test
     void PrimeNumbersShouldBePrime() {
-        assertTrue(IsPrime.isPrime(2), "2 should be prime");
-        assertTrue(IsPrime.isPrime(3), "3 should be prime");
-        assertTrue(IsPrime.isPrime(5), "5 should be prime");
-        assertTrue(IsPrime.isPrime(7), "7 should be prime");
-        assertTrue(IsPrime.isPrime(11), "11 should be prime");
-        assertTrue(IsPrime.isPrime(13), "13 should be prime");
+        assertAll(
+                () -> assertTrue(IsPrime.isPrime(2), "2 should be prime"),
+                () -> assertTrue(IsPrime.isPrime(3), "3 should be prime"),
+                () -> assertTrue(IsPrime.isPrime(5), "5 should be prime"),
+                () -> assertTrue(IsPrime.isPrime(7), "7 should be prime"),
+                () -> assertTrue(IsPrime.isPrime(11), "11 should be prime"),
+                () -> assertTrue(IsPrime.isPrime(13), "13 should be prime"),
+                () -> assertTrue(IsPrime.isPrime(17), "17 should be prime"),
+                () -> assertTrue(IsPrime.isPrime(19), "19 should be prime"),
+                () -> assertTrue(IsPrime.isPrime(97), "297 should be prime"),
+                () -> assertTrue(IsPrime.isPrime(101), "101 should be prime"),
+                () -> assertTrue(IsPrime.isPrime(1003411), "1003411 should be prime")
+        );
     }
 }
