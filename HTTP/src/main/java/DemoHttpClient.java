d7612279010f7e9af49f9b6d543da6538741c676
LugrenLaxx <lugrenl@gmail.com>
2025-07-18 00:09:13 +0300

    fix_DemoHttpClient

--- HTTP/src/main/java/DemoHttpClient.java
+++ HTTP/src/main/java/DemoHttpClient.java
@@ -1,5 +1,3 @@
-package networking;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -8,15 +6,16 @@ import java.net.http.HttpResponse;
 
 public class DemoHttpClient {
   public static void main(String[] args) throws IOException, InterruptedException {
-    HttpClient client = HttpClient.newHttpClient();
-    HttpRequest request = HttpRequest.newBuilder()
-      .uri(URI.create("https://api.exchangeratesapi.io/latest?symbols=USD"))
-      .build();
+    try (HttpClient client = HttpClient.newHttpClient()) {
+      HttpRequest request = HttpRequest.newBuilder()
+        .uri(URI.create("https://api.exchangeratesapi.io/latest?symbols=USD"))
+        .build();
 
-    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+      HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-    System.out.println(response.body());
-    System.out.println(response.headers().map());
-    System.out.println(response.statusCode());
+      System.out.println(response.body());
+      System.out.println(response.headers().map());
+      System.out.println(response.statusCode());
+    }
   }
 }
